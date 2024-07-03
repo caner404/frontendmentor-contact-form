5ca0c6e48addbb80bb7c730a668f06e35e39181b
Caner Iskenderoglu <caner.iskenderoglu@gmx.de>
2024-07-03 20:09:25 +0200

    fix name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frontendmentorproject-contact-form/',
+  base: '/frontendmentor-contact-form/',
 });
